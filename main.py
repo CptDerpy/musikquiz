6cde5f3f8d03b0702a6843103009d561f7912b12
August <august.birk.olsen@gmail.com>
2019-06-18 15:26:26 +0200

    asd

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import flaskenv
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("music.html")
+
+@app.route("/music")
+def music():
+	url = "https://www.youtube.com/watch?v=3EmUmbhDRiY"
+	video = pafy.new(url)
+	audio = video.getbestaudio()
+	audio_url = audio.url
+	return render_template("music.html", audio_url=audio_url)
+
+if __name__ == "__main__":
+	app.run(debug=True)
