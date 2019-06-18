f90aac40aa7c24c9776cecdabee34b198293e29c
Lukas Nyboe Bek <lnb@netcompany.com>
2019-06-18 14:59:01 +0200

    i fix maybe break

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\augus\OneDrive\Skrivebord\Musikquiz\musikquiz\venv"
+$env:VIRTUAL_ENV="C:\Projects\musikquiz\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
